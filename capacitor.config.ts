0b84cb55bd4ddedc6d7e4e4d300d223508088fa4
Onell Dishmey <Onelldish@hotmail.com>
2024-04-08 04:10:53 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app_defensa_civil',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
